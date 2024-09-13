43b52469841eea70fa2fbe4ec800bb874985a94d
Stefano Carraro <stefano.c@edu.pucrs.br>
2024-09-13 19:15:51 -0300

    Added timestamps and frequency graphics to comment authors

--- app.py
+++ app.py
@@ -9,17 +9,9 @@ st.session_state['comments_json'] = 'comments.json'
 st.session_state['partitions'] = get_partitions(st.session_state['comments_json'])
 
 def landing_page():
-    st.title('Análise de chat')
+    st.title('Em progresso')
 
-    if st.button('Iniciar análise'):
-        analyze_chat(st.session_state['comments_json'])
-
-def analyze_chat(comments_json):
-    if comments_json is not None:
-        comments_data = file_to_json(comments_json)
-        gerar_nuvem_palavras(comments_data)
-    else:
-        st.error('Faça o upload do arquivo de comentários')
+    st.write('Selecione uma das opções no menu lateral para visualizar as análises')
 
 def comments_peak():
     st.title('Picos de Comentários')
@@ -40,9 +32,10 @@ def most_comments():
         authors = get_top_authors(st.session_state['comments_json'], n=n_authors)
         for author, count in authors:
             with st.expander(f'{author}: {count} comentários'):
-                comments = get_author_comments(author, st.session_state['comments_json'])
+                path, comments = get_author_comments(author, st.session_state['comments_json'])
+                st.image(path)
                 for comment in comments:
-                    st.write(comment)
+                    st.write(f"{comment['time_elapsed']} - {comment['message']}")
 
 
     else:
@@ -59,6 +52,25 @@ def show_partitions():
             st.write(f'Fim: {partition["end"]}')
             st.image(gerar_nuvem_palavras(partition['comments'], complemento=f'_particao_{index}'))
 
+def show_stats():
+    st.title('Estatísticas')
+    comments_data = file_to_json(st.session_state['comments_json'])
+    st.write(f'Número total de comentários: {len(comments_data)}')
+    st.write(f'Número total de pessoas que comentaram: {len(set([comment["author"] for comment in comments_data]))}')
+    st.write(f'Média de comentários por pessoas: {len(comments_data) / len(set([comment["author"] for comment in comments_data]))}')
+    st.write(f'Número total de palavras: {sum([len(comment["message"].split()) for comment in comments_data])}')
+    st.write(f'Número total de palavras únicas: {len(set([word for comment in comments_data for word in comment["message"].split()]))}')
+    st.write(f'Média de palavras por comentário: {sum([len(comment["message"].split()) for comment in comments_data]) / len(comments_data)}')
+
+def show_new_members():
+    st.title('Membros')
+    member_data = file_to_json(st.session_state['comments_json'])
+    path, members = get_new_members(member_data)
+    st.image(path)
+    with st.expander('Novos membros', expanded=True):
+        for member in members:
+            st.write(f'{member["author"]} - {member["time_elapsed"]}')
+
 pagina = st.sidebar.selectbox('Página', ['Upload Json','Picos de Comentarios', 'Maiores comentaristas', 'Particoes', 'Estátisticas', 'Membros'])
 
 if pagina == 'Picos de Comentarios':
@@ -68,22 +80,8 @@ elif pagina == 'Maiores comentaristas':
 elif pagina == 'Particoes':
     show_partitions()
 elif pagina == 'Estátisticas':
-    st.title('Estatísticas')
-    comments_data = file_to_json(st.session_state['comments_json'])
-    st.write(f'Número total de comentários: {len(comments_data)}')
-    st.write(f'Número total de pessoas que comentaram: {len(set([comment["author"] for comment in comments_data]))}')
-    st.write(f'Média de comentários por pessoas: {len(comments_data) / len(set([comment["author"] for comment in comments_data]))}')
-    st.write(f'Número total de palavras: {sum([len(comment["message"].split()) for comment in comments_data])}')
-    st.write(f'Número total de palavras únicas: {len(set([word for comment in comments_data for word in comment["message"].split()]))}')
-    st.write(f'Média de palavras por comentário: {sum([len(comment["message"].split()) for comment in comments_data]) / len(comments_data)}')
+    show_stats()
 elif pagina == 'Membros':
-    st.title('Membros')
-    member_data = file_to_json(st.session_state['comments_json'])
-    path, members = get_new_members(member_data)
-    st.image(path)
-    with st.expander('Novos membros', expanded=True):
-        for member in members:
-            st.write(f'{member["author"]} - {member["time_elapsed"]}')
-
+    show_new_members()
 else:
     landing_page()
--- stats.py
+++ stats.py
@@ -1,18 +1,40 @@
 import json
+from matplotlib import pyplot as plt
+from matplotlib import dates as mdates
+import pandas as pd
 from collections import Counter
 
-def get_author_comments(author, comments_json):
+def get_author_comments(author, comments_json, interval=30):
     with open(comments_json, 'r') as file:
         data = json.load(file)
 
     author_comments = []
     for item in data:
         if item['author'] == author:
-            author_comments.append(item['message'])
+            author_comments.append(item)
     
-    author_filtered_comments = [comment for comment in author_comments if comment.strip()]
+    author_filtered_comments = [comment for comment in author_comments if comment['message'].strip()]
 
-    return author_filtered_comments
+    df = pd.DataFrame(author_filtered_comments)
+    df['timestamp'] = pd.to_datetime(df['time_elapsed'])
+    df.set_index('timestamp', inplace=True)
+    resampled_df = df.resample(f'{interval}T').size()
+
+    plt.figure(figsize=(14, 6))
+    plt.plot(resampled_df.index, resampled_df.values)
+    plt.xlabel('Tempo')
+    plt.ylabel('Comentários')
+    plt.title(f'Comentários a cada {interval} minutos')
+
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
+
+    plt.grid(True)
+
+    path = f'output/comentarios_{author}_por_minuto.png'
+
+    plt.savefig(path)
+
+    return path, author_filtered_comments
 
 def get_top_authors(comments_json, n=5):
     with open(comments_json, 'r') as file:
